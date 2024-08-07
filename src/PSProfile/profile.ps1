2e86cd82dda53fd4d3787e129ff9b434511cc0e9
jpbruckler <jpbruckler@gmail.com>
2024-08-07 02:37:08 -0400

    chore: moving files around

--- src/PSProfile/profile.ps1
+++ src/PSProfile/profile.ps1
@@ -1,7 +1,14 @@
-Set-PSResourceRepository -Name PSGallery -Trusted
-PowerShellGet\Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-
-$Src = Join-Path -Path $PSScriptRoot -ChildPath 'Profile'
-if (-not(Test-Path -Path $Src)) {
-    $null = New-Item -Path $Src -ItemType Directory -Force
+if (-not((Get-PSResourceRepository -Name PSGallery).Trusted)) {
+    Set-PSResourceRepository -Name PSGallery -Trusted
 }
+
+if ((PowerShellGet\Get-PSRepository -Name PSGallery).InstallationPolicy -ne 'Trusted') {
+    PowerShellGet\Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+}
+
+$src = Join-Path -Path $PSScriptRoot -ChildPath 'Profile'
+if (-not(Test-Path -Path $src)) {
+    $null = New-Item -Path $src -ItemType Directory -Force
+}
+
+. (Join-Path -Path $src -ChildPath 'functions.ps1')
---
+++ src/PSProfile/profile/functions.ps1
@@ -0,0 +1,8 @@
+<#
+.SYNOPSIS
+    This file contains all functions that should be loaded into the PowerShell
+    session.
+.DESCRIPTION
+    This file contains all functions that should be loaded into the PowerShell
+    session. It is loaded by $env:USERPROFILE\Documents\PowerShell\profile.ps1
+#>
